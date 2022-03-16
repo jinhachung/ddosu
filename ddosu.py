a47212cc83f37020917b5aade9567b94fc8124bf
gobblygobble <gobblygobble@kaist.ac.kr>
2022-03-16 11:45:37 +0900

    add ddosu.py

---
+++ ddosu.py
@@ -0,0 +1,62 @@
+import os
+import requests
+from datetime import date
+
+def str_no_num(s):
+    nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for num in nums:
+        if num in s:
+            return False
+    return True
+
+def main():
+    # get date
+    today = date.today()
+    d = today.strftime("%Y-%m-%d")
+    url = "https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd=emp&stt_dt=" + d
+    r = requests.get(url, allow_redirects=True)
+    print("Downloading request items' information on: {}".format(d))
+
+    # save content with name
+    f = open(str(d), "wb")
+    f.write(r.content)
+    f.close()
+    f = open(str(d), "r")
+    lines = f.readlines()
+    f.close()
+    
+    menu = [[], []]
+    p = False
+    ind = 0
+    for l in lines:
+        # printing --> check if we keep printing
+        if p:
+            if "</ul>" in l:
+                p = False
+                ind = 1
+            else:
+                menu[ind].append(l)
+                p = True # redundant
+        # not printing --> check if we start printing
+        else:
+            if ("list-1st" in l) and ("!" not in l):
+                p = True
+            else:
+                p = False
+    
+    print("Lunch: ")
+    for m in menu[0]:
+        _m = m.strip().strip("<br/>")
+        if (str_no_num(_m)) and (len(_m) > 0):
+            print(_m)
+
+    print("\nDinner: ")
+    for m in menu[1]:
+        _m = m.strip().strip("<br/>")
+        if (str_no_num(_m)) and (len(_m) > 0):
+            print(_m)
+    
+    os.remove(str(d))
+
+if __name__ == "__main__":
+    main()
